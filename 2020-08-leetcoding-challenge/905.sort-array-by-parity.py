7588fa143b1456f37a1b9bbd6c947bd8070659cc
ln_north <yamahisa.kita@gmail.com>
2020-08-24 00:19:41 +0900

    2020-08-24

--- 2020-08-leetcoding-challenge/905.sort-array-by-parity.py
+++ 2020-08-leetcoding-challenge/905.sort-array-by-parity.py
@@ -9,15 +9,7 @@ class Solution:
     def sortArrayByParity(self, A: List[int]) -> List[int]:
         even = list(filter(lambda x : x % 2 == 0, A))
         odd = list(filter(lambda x : x % 2 != 0, A))
-
-        result = []
-
-        for i in range(len(A)):
-            if i % 2 == 0:
-                result.append(even[int(i / 2)])
-            else:
-                result.append(odd(int((i - 1) / 2)))
         
-        return result
+        return even + odd
 # @lc code=end
 
